9a3014dccd0110d90496bf3f2c9ff847fbe372f1
Sakshi Torgalmath <sakshitmath@gmail.com>
2026-05-01 14:59:41 +0530

    fix: add Vercel URL to CORS config

--- src/main/java/com/meterflow/backend/config/SecurityConfig.java
+++ src/main/java/com/meterflow/backend/config/SecurityConfig.java
@@ -54,9 +54,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173",
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
                 "http://localhost:5174",
-                "http://localhost:3000"));
+                "http://localhost:3000",
+                "https://meterflow-frontend-git-main-sakshitmaths-projects.vercel.app",
+                "https://meterflow-frontend-ol6x4gz7v-sakshitmaths-projects.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET","POST","PUT",
                 "PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
